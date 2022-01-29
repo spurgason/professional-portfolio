810c6159b89841d6bce76b4b2941256b09405cc1
spurgason <shanepurgason.98@gmail.com>
2022-01-29 13:08:42 -0500

    added contact page

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,5 +1,21 @@
 import "./contact.scss";
 
 export default function Contact() {
-  return <div className="contact" id="contact"></div>;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  }
+
+  return <div className="contact" id="contact">
+    <div className="left">
+      <img src="assets/sputnik.svg" alt="contact icon"/>
+    </div>
+    <div className="right">
+      <h2>Contact</h2>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="Email"/>
+        <textarea placeholder="Message"></textarea>
+        <button type="submit">Send</button>
+      </form>
+    </div>
+  </div>;
 }
